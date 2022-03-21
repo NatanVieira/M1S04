036c4317550785cf0aa2b06561c3772f499428b1
Natanael Vieira <natanaelvieira130@gmail.com>
2022-03-21 17:08:31 -0300

    M1S04 - Exercicio 02

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Exercicio 1
 let classificaIdade = () => {
     let idade = prompt("Digite sua idade: ");
     let classificacao = '';
@@ -8,4 +9,34 @@ let classificaIdade = () => {
     else
         classificacao = 'Idoso';
     console.log("Classificacao: " + classificacao);
+}
+
+// Exercicio 2
+function operacoesMatematicas (n1, n2, operador) {
+    let resultado;
+    switch(operador){
+        case '+':
+            resultado = n1 + n2;
+            break;
+        case '-':
+            resultado = n1 - n2;
+            break;
+        case '*':
+            resultado = n1 * n2;
+            break;
+        case '/':
+            resultado = n1 / n2;
+            break;
+        default:
+            resultado = 'Operação não disponível';
+    }
+    console.log(resultado);
+}
+
+function realizaOperacoesMatematicas () {
+    operacoesMatematicas(4,4,'+');
+    operacoesMatematicas(5,2,'-');
+    operacoesMatematicas(4,4,'*');
+    operacoesMatematicas(8,4,'/');
+    operacoesMatematicas(4,4,'c');
 }
