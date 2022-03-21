dc4a8b45195e2d4a6c03b063993cd9c4d3190563
Natanael Vieira <natanaelvieira130@gmail.com>
2022-03-21 19:20:50 -0300

    M1S04 - Exercicio 9

--- script.js
+++ script.js
@@ -92,3 +92,22 @@ function converteTemperatura() {
     console.log(temperaturaF);
     console.log(temperaturaC);
 }
+
+//Exercicio 8
+
+function manipulaArray(opcao) {
+    let cidades = [
+        { nome: 'Patos de Minas', populacao: 153585 },
+        { nome: 'Lages', populacao: 157349 },
+        { nome: 'Ibiúna', populacao: 79479 },
+        { nome: 'Maringá', populacao: 403063 },
+        { nome: 'Curitiba', populacao: 1963726 },
+        { nome: 'Florianópolis', populacao: 508826 },
+        { nome: 'Pato Branco', populacao: 84779 },
+        ];
+    if (opcao === 1)
+        console.log(cidades.filter(a => a.populacao >= 200000).reverse());
+    else if (opcao === 2)
+        console.log(cidades.find(a => a.nome.toLowerCase() === 'florianópolis'));
+
+}
